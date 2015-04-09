4fc08069c989d0c72380d23bee9e1b30301c2208
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2015-04-09 20:39:50 +0300

    adding emerald ski

---
+++ Skins/Emerald/Source/SapphireSkin.cs
@@ -0,0 +1,24 @@
+using ICities;
+
+namespace SapphireSkin
+{
+
+    public class Mod : IUserMod
+    {
+
+        public string Name
+        {
+            get
+            {
+                return "Emerald";
+            }
+        }
+
+        public string Description
+        {
+            get { return "Emerald skin for Sapphire"; }
+        }
+
+    }
+
+}
